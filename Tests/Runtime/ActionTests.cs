4d139147947521e829ad9950c59200ab581b0adc
Vladimir Popov <zor1994@gmail.com>
2020-10-18 16:38:47 +0300

    Action tests

---
+++ Tests/Runtime/ActionTests.cs
@@ -0,0 +1,49 @@
+// Copyright (c) 2020 Vladimir Popov zor1994@gmail.com https://github.com/ZorPastaman/Behavior-Tree
+
+using NUnit.Framework;
+using Zor.BehaviorTree.Builder;
+using Zor.BehaviorTree.Core;
+using Zor.BehaviorTree.Core.Actions;
+using Zor.SimpleBlackboard.Core;
+
+namespace Zor.BehaviorTree.Tests
+{
+	public static class ActionTests
+	{
+		[Test]
+		public static void SetClassValueTest()
+		{
+			var propertyName = new BlackboardPropertyName("value");
+			const string value = "value";
+			var blackboard = new Blackboard();
+			var treeBuilder = new TreeBuilder();
+			treeBuilder.AddBehavior<SetClassValue<string>>(value, propertyName).Finish();
+			Tree tree = treeBuilder.Build(blackboard);
+			tree.Initialize();
+
+			Assert.AreEqual(Status.Success, tree.Tick());
+			Assert.IsTrue(blackboard.TryGetClassValue(propertyName, out string val));
+			Assert.AreEqual(value, val);
+
+			tree.Dispose();
+		}
+
+		[Test]
+		public static void SetStructValueTest()
+		{
+			var propertyName = new BlackboardPropertyName("value");
+			const int value = -2;
+			var blackboard = new Blackboard();
+			var treeBuilder = new TreeBuilder();
+			treeBuilder.AddBehavior<SetStructValue<int>>(value, propertyName).Finish();
+			Tree tree = treeBuilder.Build(blackboard);
+			tree.Initialize();
+
+			Assert.AreEqual(Status.Success, tree.Tick());
+			Assert.IsTrue(blackboard.TryGetStructValue(propertyName, out int val));
+			Assert.AreEqual(value, val);
+
+			tree.Dispose();
+		}
+	}
+}
