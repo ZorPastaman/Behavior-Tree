be441db4e7bd97efdb14df187054c0769b3b5c1d
Vladimir Popov <zor1994@gmail.com>
2020-10-18 16:30:46 +0300

    SequenceTest

--- Tests/Runtime/CompositeTests.cs
+++ Tests/Runtime/CompositeTests.cs
@@ -455,5 +455,68 @@ namespace Zor.BehaviorTree.Tests
 
 			tree.Dispose();
 		}
+
+		[Test]
+		public static void SequenceTest()
+		{
+			const string currentIndexFieldName = "m_currentChildIndex";
+			const string rootFieldName = "m_root";
+
+			FieldInfo currentIndexField = typeof(Sequence).GetField(currentIndexFieldName,
+				BindingFlags.NonPublic | BindingFlags.Instance);
+			FieldInfo rootField = typeof(Tree).GetField(rootFieldName,
+				BindingFlags.NonPublic | BindingFlags.Instance);
+
+			var propertyNames = new[]
+			{
+				new BlackboardPropertyName("value0"),
+				new BlackboardPropertyName("value1"),
+				new BlackboardPropertyName("value2"),
+				new BlackboardPropertyName("value3"),
+				new BlackboardPropertyName("value4")
+			};
+
+			var blackboard = new Blackboard();
+			blackboard.SetStructValue(propertyNames[0], Status.Success);
+			blackboard.SetStructValue(propertyNames[1], Status.Success);
+			blackboard.SetStructValue(propertyNames[2], Status.Success);
+			blackboard.SetStructValue(propertyNames[3], Status.Success);
+			blackboard.SetStructValue(propertyNames[4], Status.Success);
+
+			var treeBuilder = new TreeBuilder();
+			treeBuilder.AddBehavior<Sequence>()
+				.AddBehavior<VariableBehavior>(propertyNames[0]).Finish()
+				.AddBehavior<VariableBehavior>(propertyNames[1]).Finish()
+				.AddBehavior<VariableBehavior>(propertyNames[2]).Finish()
+				.AddBehavior<VariableBehavior>(propertyNames[3]).Finish()
+				.AddBehavior<VariableBehavior>(propertyNames[4]).Finish()
+			.Finish();
+			Tree tree = treeBuilder.Build(blackboard);
+			object root = rootField.GetValue(tree);
+			tree.Initialize();
+
+			Assert.AreEqual(Status.Success, tree.Tick());
+			Assert.AreEqual(5, currentIndexField.GetValue(root));
+
+			blackboard.SetStructValue(propertyNames[1], Status.Running);
+			Assert.AreEqual(Status.Running, tree.Tick());
+			Assert.AreEqual(1, currentIndexField.GetValue(root));
+
+			blackboard.SetStructValue(propertyNames[2], Status.Failure);
+			blackboard.SetStructValue(propertyNames[3], Status.Failure);
+			blackboard.SetStructValue(propertyNames[4], Status.Failure);
+			Assert.AreEqual(Status.Running, tree.Tick());
+			Assert.AreEqual(1, currentIndexField.GetValue(root));
+
+			blackboard.SetStructValue(propertyNames[1], Status.Success);
+			Assert.AreEqual(Status.Failure, tree.Tick());
+			Assert.AreEqual(2, currentIndexField.GetValue(root));
+
+			blackboard.SetStructValue(propertyNames[2], Status.Error);
+			Assert.AreEqual(Status.Error, tree.Tick());
+			Assert.AreEqual(2, currentIndexField.GetValue(root));
+
+			tree.Dispose();
+		}
 	}
 }
