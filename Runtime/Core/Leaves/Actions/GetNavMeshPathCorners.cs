3b8fd2f836a58d8f7180510d9a0cdbce5312fda4
Vladimir Popov <zor1994@gmail.com>
2021-11-11 10:23:11 +0300

    NavMeshPath behaviors

---
+++ Runtime/Core/Leaves/Actions/GetNavMeshPathCorners.cs
@@ -0,0 +1,48 @@
+﻿// Copyright (c) 2020-2021 Vladimir Popov zor1994@gmail.com https://github.com/ZorPastaman/Behavior-Tree
+
+using System.Runtime.CompilerServices;
+using UnityEngine.AI;
+using Zor.BehaviorTree.DrawingAttributes;
+using Zor.SimpleBlackboard.Core;
+
+namespace Zor.BehaviorTree.Core.Leaves.Actions
+{
+	public sealed class GetNavMeshPathCorners : Action,
+		ISetupable<BlackboardPropertyName, BlackboardPropertyName>, ISetupable<string, string>
+	{
+		[BehaviorInfo] private BlackboardPropertyName m_pathPropertyName;
+		[BehaviorInfo] private BlackboardPropertyName m_cornersPropertyName;
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		void ISetupable<BlackboardPropertyName, BlackboardPropertyName>.Setup(BlackboardPropertyName pathPropertyName,
+			BlackboardPropertyName cornersPropertyName)
+		{
+			SetupInternal(pathPropertyName, cornersPropertyName);
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		void ISetupable<string, string>.Setup(string pathPropertyName, string cornersPropertyName)
+		{
+			SetupInternal(new BlackboardPropertyName(pathPropertyName),
+				new BlackboardPropertyName(cornersPropertyName));
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		private void SetupInternal(BlackboardPropertyName pathPropertyName, BlackboardPropertyName cornersPropertyName)
+		{
+			m_pathPropertyName = pathPropertyName;
+			m_cornersPropertyName = cornersPropertyName;
+		}
+
+		protected override Status Execute()
+		{
+			if (blackboard.TryGetClassValue(m_pathPropertyName, out NavMeshPath path) & path != null)
+			{
+				blackboard.SetClassValue(m_cornersPropertyName, path.corners);
+				return Status.Success;
+			}
+
+			return Status.Error;
+		}
+	}
+}
---
+++ Runtime/Serialization/SerializedBehaviors/Leaves/Actions/SerializedGetNavMeshPathCorners.cs
@@ -0,0 +1,13 @@
+﻿// Copyright (c) 2020-2021 Vladimir Popov zor1994@gmail.com https://github.com/ZorPastaman/Behavior-Tree
+
+using Zor.BehaviorTree.Core.Leaves.Actions;
+using Zor.BehaviorTree.DrawingAttributes;
+
+namespace Zor.BehaviorTree.Serialization.SerializedBehaviors.Leaves.Actions
+{
+	[NameOverride("Path Property Name", 0), NameOverride("Corners Property Name", 1)]
+	[SearchGroup("Nav Mesh Path")]
+	public sealed class SerializedGetNavMeshPathCorners : SerializedAction<GetNavMeshPathCorners, string, string>
+	{
+	}
+}
